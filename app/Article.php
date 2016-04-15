1064292bb517cf8768c3e498f105ed03d23dd550
ronaldlunaramos <ronaldwin79@hotmail.com>
2016-04-15 10:30:10 -0400

    add scope article category

---
+++ app/Article.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\SluggableInterface;
+use Cviebrock\EloquentSluggable\SluggableTrait;
+class Article extends Model implements SluggableInterface
+{
+    use SluggableTrait;
+    protected $sluggable= [
+    'build_from'=>'title',
+    'save_to'=>'slug',
+    ];
+
+    Protected $table ="articles";
+    Protected $fillable=['title','content','category_id','user_id'];
+
+    public function category() {
+    	return $this->belongsTo('App\Category');
+    }
+    public function user() {
+    	return $this->belongsTo('App\User');
+    }
+    public function image() {
+    	return $this->hasMany('App\Image');
+    }
+    public function tags() {
+    	return $this->belongsToMany('App\Tag')->withTimestamps();
+    }
+    public function scopeSearch($query,$title) {
+        return $query->where('title','LIKE',"%$title%");
+    }
+}
