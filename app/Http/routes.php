cf712a3cbd9cb70d30bd2c2b4d58fa48bd6d517d
ronaldlunaramos <ronaldwin79@hotmail.com>
2016-04-15 10:20:40 -0400

    add middlewares

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 se pueden tener rutas GET PUT POST DELETE 
 */
@@ -7,9 +6,58 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix'=>'admin'], function () {
+//Route::group(['prefix'=>'admin','middleware'=>'auth'], function () {
+Route::group(['prefix'=>'admin','middleware'=>'auth'], function () {
+
+	Route::get('/',['as'=>'admin.index',function () {
+		return view('welcome');
+	}]);
+
 	Route::resource('users','UsersController');
+	Route::get('users/{id}/destroy',[
+		'uses'=>'UsersController@destroy',
+		'as'=>'admin.users.destroy'
+		]);
+
+	Route::resource('categories','CategoriesController');
+	Route::get('categories/{id}/destroy',[
+		'uses'=>'CategoriesController@destroy',
+		'as'=>'admin.categories.destroy'
+		]);
+
+	Route::resource('tags','TagsController');
+	Route::get('tags/{id}/destroy',[
+		'uses'=>'TagsController@destroy',
+		'as'=>'admin.tags.destroy'
+		]);
+
+	Route::resource('articles','ArticlesController');
+	Route::get('articles/{id}/destroy',[
+		'uses'=>'ArticlesController@destroy',
+		'as'=>'admin.articles.destroy'
+		]);
+	//Route::get('users')
+	
 });
+
+Route::get('admin/auth/login', [
+	'uses'=>'Auth\AuthController@getLogin',
+	'as'=>'admin.auth.login'
+	]);
+
+Route::post('admin/auth/login',[
+	'uses'=>'Auth\AuthController@postLogin',
+	'as'=>'admin.auth.login'
+	]);
+
+Route::get('admin/auth/logout',[
+	'uses'=>'Auth\AuthController@getLogout',
+	'as'=>'admin.auth.logout'
+	]);
+
+	
+
+
 
 /**Route::group(['prefix'=>'admin'], function() {
 	Route::resource('user','UsersController');
